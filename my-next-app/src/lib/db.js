ea3fbdfce050ad777ae794c349383fd60eeea7ff
mangesh <kmangesh931@gmail.com>
2025-09-03 20:42:16 +0530

    update db

--- my-next-app/src/lib/db.js
+++ my-next-app/src/lib/db.js
@@ -15,24 +15,23 @@
 //     , // Verify server certificate
 //   },
 // });
-
 import mysql from "mysql2/promise";
 
 let sslConfig = false;
 
 if (process.env.NODE_ENV === "production" && process.env.DB_CA) {
   sslConfig = {
-    ca: process.env.DB_CA.replace(/\\n/g, "\n"), // handle multiline
+    ca: process.env.DB_CA.replace(/\\n/g, "\n"),
   };
 }
 
-export const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   ssl: sslConfig,
 });
+export { pool }; 
 
